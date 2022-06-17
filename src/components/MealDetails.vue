e2efb4496c3e8be2650efc3721f38c89039865b7
Geraldine <geraldine.orellana@gmail.com>
2022-06-16 21:26:35 -0400

    recetas por categoria

--- src/components/MealDetails.vue
+++ src/components/MealDetails.vue
@@ -1,10 +1,40 @@
 <template>
-  
+
+<div>
+    <h3> {{ meal_title }} </h3>
+    
+</div>
+
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
+
+export default {
+    name: "MealDetails",
+
+    data(){
+        return {
+            meal_title: "Titulo de la receta" //this.$route.params.title,
+
+        }
+    },
+    mounted() {
+     
+    
+       axios
+      .get("http://themealdb.com/api/json/v1/1/lookup.php?i=" + this.$route.params)
+      .then((res) => {
+    //   this.meals = res.data.meals;
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    }
+  
+    
 }
 </script>
 
